0541b22b5874a49ed409b5aff370fb6d7b8d663f
Raymond Wen <raymondwen@Raymonds-MacBook-Air.local>
2026-03-06 13:31:23 +1030

    feat: translate payroll to C#

---
+++ Week1/PayrollCalculator/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program{
+
+    const double TAX_RATE = 0.2;
+
+    static double calculate_pay(double hours, double rate){
+        if(hours < 0 || hours < 0){
+            throw new Exception("Hours and Rate must be positive");
+        }
+        double gross = hours*rate;
+        double tax = gross*TAX_RATE;
+        double net = gross - tax;
+        return net;
+        
+    }
+
+    static void Main(string[] args){
+        double hours = 0;
+        double rate = 0;
+        Console.WriteLine("Enter employee name: ");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Hours worked: ");
+        try{
+            hours = double.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+            {
+                Console.WriteLine("Please only enter a number");
+            }
+
+        Console.WriteLine("Hourly rate: ");
+        try{
+            rate = double.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+            {
+                Console.WriteLine("Please only enter a number");
+            }
+
+        double net_pay = calculate_pay(hours, rate);
+
+        string msg = $"{name} earned ${net_pay} after tax";
+
+        Console.WriteLine(msg);
+
+    }
+}
+
+
+
+
